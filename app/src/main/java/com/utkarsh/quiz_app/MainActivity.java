d22ff4ceff71f840258a874a484ccd868cdfaaa7
UtkarshDubey0242 <utkarshdubey0242@gmail.com>
2022-07-05 23:50:31 +0530

    Initial commit

---
+++ app/src/main/java/com/utkarsh/quiz_app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.utkarsh.quiz_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,DashboardActivity.class);
+                startActivity(intent);
+            }
+        }, 100);
+    }
+}
